e598e4fdbc9bd382a905a71ac5687f2a579d5987
Ujval-Shrivathsa <129066873+Ujval-Shrivathsa@users.noreply.github.com>
2024-04-15 09:08:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function runfunction(){
+    const closedisplaynone = document.querySelector('.closedisplaynone');
+    closedisplaynone.style.display = 'flex';
+    const thirdPage = document.querySelector('.thirdPage');
+    thirdPage.style.height = '130vh';
+    const seeMore = document.querySelector('.seeMore');
+    seeMore.style.display = 'none';
+    const nonedisplay = document.querySelector('.nonedisplay');
+    nonedisplay.style.display = 'block';
+}
+
+function runfunction1(){
+    const closedisplaynone = document.querySelector('.closedisplaynone');
+    closedisplaynone.style.display = 'none';
+    const thirdPage = document.querySelector('.thirdPage');
+    thirdPage.style.height = '90vh';
+    const nonedisplay = document.querySelector('.nonedisplay');
+    nonedisplay.style.display = 'none';
+    const therikaljf = document.querySelector('.therikaljf');
+    therikaljf.style.display = 'block';
+}
